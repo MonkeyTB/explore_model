72eded5285bb4ea619d1d10f1a141d26bccba9aa
MonkeyTB <13689446615@163.com>
2021-11-29 23:30:19 +0800

    keras function

---
+++ keras/Throry/5_Function.py
@@ -0,0 +1,106 @@
+#_Author_:Monkey
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from tensorflow.keras.layers import Input, Dense, Flatten, Masking
+from tensorflow.keras.models import Sequential, Model
+layer = Dense(32)
+config = layer.get_config()
+print( config ) # 以含有Numpy矩阵的列表形式返回层的权重
+print( layer.get_weights() ) # 以含有Numpy矩阵的列表形式返回层的权重
+print( Dense.from_config(config))
+
+# 2. Dense
+'''
+Dense(units, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
+input: nD张量，尺寸（batch_size, ..., input_dim）
+output:nD张量，尺寸（batch_size, ..., units）
+'''
+
+# 3. Fallent
+'''
+keras.layers.Flatten(data_format=None)
+data_format:一个字符串，其值为channel_last（默认）或者channel_first。它表明输入的维度的顺序。此参数的目的是为了当模型
+从一个数据格式切换到另一个数据格式时保留权重顺序。
+'''
+model = Sequential()
+model.add(Conv2D(64, (3, 3),
+                 input_shape=(3, 32, 32), padding='same',))
+# 现在：model.output_shape == (None, 64, 32, 32)
+model.add(Flatten())
+# 现在：model.output_shape == (None, 65536)
+
+# 4. Permute
+'''
+keras.layers.Permute(dims)
+根据给定的模式置换输入的维度
+dims：整数元组，置换模式，不包含样本的维度。索引从1开始，例如（2，1），就是置换第一维和第二维
+输入尺寸：任意
+输出尺寸：输入尺寸
+
+model = Sequential()
+model.add(Permute((2, 1), input_shape=(10, 64)))
+# 现在： model.output_shape == (None, 64, 10)
+# 注意： `None` 是批表示的维度
+'''
+# 5. RepeatVector
+'''
+keras.layers.RepeatVector(n)
+:将重复n次
+n：整数，输入的次数
+输入：2D张量
+输出：3D张量
+model = Sequential()
+model.add(Dense(32, input_dim=32))
+# 现在： model.output_shape == (None, 32)
+# 注意： `None` 是批表示的维度
+model.add(RepeatVector(3))
+# 现在： model.output_shape == (None, 3, 32)
+'''
+
+# 6. Lambda
+'''
+keras.layers.Lambda(function, output_shape=None, mask=None, arguments=None)
+将任意表达式封装成layer对象
+function: 需要封装的函数。 将输入张量作为第一个参数。
+输入尺寸:任意
+输出尺寸:由 output_shape 参数指定 (或者在使用 TensorFlow 时，自动推理得到)。
+# 添加一个 x -> x^2 层
+model.add(Lambda x : x**2)
+
+# 添加一个网络层，返回输入的正数部分
+# 与负数部分的反面的连接
+
+def antirectifier(x):
+    x -= K.mean(x, axis=1, keepdims=True)
+    x = K.l2_normalize(x, axis=1)
+    pos = K.relu(x)
+    neg = K.relu(-x)
+    return K.concatenate([pos, neg], axis=1)
+
+def antirectifier_output_shape(input_shape):
+    shape = list(input_shape)
+    assert len(shape) == 2  # only valid for 2D tensors
+    shape[-1] *= 2
+    return tuple(shape)
+model.add(Lambda(antirectifier,
+                 output_shape=antirectifier_output_shape))
+'''
+
+# 7. Masking
+'''
+keras.layers.Masking(mask_value=0.0)
+使用覆盖值覆盖序列,以跳过时间步
+对于输入张量的每一个时间步(张量的第一个维度),如果所有时间步输入张量的值与mask_value相等,那么这个时间步将再下游层被
+覆盖(跳过)(只要他们支持覆盖)
+不支持覆盖则会抛出异常
+
+例:
+考虑将要喂入一个 LSTM 层的 Numpy 矩阵 x， 尺寸为 (samples, timesteps, features)。
+你想要覆盖时间步 #3 和 #5，因为你缺乏这几个 时间步的数据。你可以：
+
+设置 x[:, 3, :] = 0. 以及 x[:, 5, :] = 0.
+在 LSTM 层之前，插入一个 mask_value=0 的 Masking 层：
+model = Sequential()
+model.add(Masking(mask_value=0., input_shape=(timesteps, features)))
+model.add(LSTM(32))
+'''
